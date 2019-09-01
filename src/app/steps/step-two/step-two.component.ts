f979d0e92a6a0fe913cb7dc0d04bc947ebd4ac26
Genadius Paleckis <genadijus.paleckis@gmail.com>
2019-09-01 14:32:25 +0100

    catchup

--- src/app/steps/step-two/step-two.component.ts
+++ src/app/steps/step-two/step-two.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CardPriority} from "../../models/card-priority.model";
 import {Card} from "../../models/card.model";
 import {CdkDragDrop} from "@angular/cdk/drag-drop";
@@ -8,22 +8,15 @@ import {CdkDragDrop} from "@angular/cdk/drag-drop";
   templateUrl: './step-two.component.html',
   styleUrls: ['./step-two.component.scss']
 })
-export class StepTwoComponent implements AfterViewInit {
+export class StepTwoComponent {
   @Input() left: CardPriority;
   @Input() right: CardPriority;
   @Output() next = new EventEmitter();
   @Output() reset = new EventEmitter();
   @Output() show = new EventEmitter<Card>();
   @Output() drop = new EventEmitter<CdkDragDrop<Array<Card>>>();
-  one;
-  two;
 
   when(expr) {
     return () => !!expr;
-  };
-
-  ngAfterViewInit() {
-    this.one = this.left.items;
-    this.two = this.right.items;
   }
 }
